efc7080956701e8fbcb73094a92ffdb10a620c69
“Patrick <pctennislessons@gmail.com>
2019-09-18 15:49:13 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+const dodger = document.getElementById('dodger')
+
+console.log(dodger.style.left)
+
+function moveDodgerLeft(){
+    dodger.style.left = `${parseInt(dodger.style.left) - 1}px`
+}   
+function moveDodgerRight(){
+    dodger.style.left = `${parseInt(dodger.style.left) + 1}px`
+}   
